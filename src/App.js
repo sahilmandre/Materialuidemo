c1ee7ea39179e3b0226ba68493ed078442d48c42
sahilmandre <sahilmandre@gmail.com>
2021-06-26 00:55:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  BottomNavigation,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant="h6">Sahil Mandre</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textSecondary"
+              gutterBottom
+            >
+              Hello, I am Sahil Mandre.
+            </Typography>
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+              distinctio fugit possimus dignissimos sunt aliquam alias ration.
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="conatined" color="secondary">
+                    See Photos
+                  </Button>
+                  <Button variant="contained" color="primary">
+                    Primary
+                  </Button>
+
+                  <Button variant="outlined" color="default">
+                    My work
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
